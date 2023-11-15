1ce1434116929993abfe4c3f96cfa5ef50e5eaf1
Chairuabda <chairuabda@gmail.com>
2023-11-15 15:20:20 +0700

    update payment

--- src/pages/Transaction/index.tsx
+++ src/pages/Transaction/index.tsx
@@ -1,24 +1,47 @@
-import {
-	Box,
-	Modal,
-	ModalOverlay,
-	ModalContent,
-	ModalHeader,
-	ModalFooter,
-	ModalBody,
-	ModalCloseButton,
-	useDisclosure,
-	Button,
-	Flex,
-} from "@chakra-ui/react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Box, Flex, Image, Text, Divider } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import toRupiah from "@develoka/angka-rupiah-js";
-import axios from "axios";
-import { IconArrowRight } from "@tabler/icons-react";
+import { useNavigate } from "react-router";
+import { useLocation } from "react-router";
+import CoffeImg from "../../assets/8485f2f23233df3900caffbd968659b3.png";
+import { IconCalendar } from "@tabler/icons-react";
+import { IconUser } from "@tabler/icons-react";
+import { CashPayment } from "./cashPayment";
+import { Payment } from "./payment";
+import { useState } from "react";
+import { PaymentSuccess } from "./paymentSuccess";
 
 export const Transaction = () => {
-	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [activePage, setIsActivePage] = useState<string>("Payment");
+	const [payment, setPayment] = useState<number>(0);
+	const navigate = useNavigate();
+	const { state } = useLocation();
+	const now = new Date();
+	const day = now.getDate();
+	const month = now.getMonth();
+	const year = now.getFullYear();
+	const time = new Intl.DateTimeFormat("default", {
+		hour12: true,
+		hour: "numeric",
+		minute: "numeric",
+	}).format(now);
+
+	const months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
 	const cart = useSelector(
 		(state: RootState) => state.CartReducer.products
 	);
@@ -26,81 +49,146 @@ export const Transaction = () => {
 		(state: RootState) => state.CartReducer.totalPrice
 	);
 
-	const totalQuantity = useSelector(
-		(state: RootState) => state.CartReducer.countCart
-	);
-
-	// console.log(totalQuantity)
-	// console.log(cart)
-	// console.log(transactionPrice)
-
-	const bayar = async (
-		totalQuantity: number,
-		transactionPrice: number,
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		cart: any
-	) => {
-		try {
-			await axios.post(`http://localhost:8080/transaction`, {
-				total_quantity: totalQuantity,
-				total_price: transactionPrice,
-				cashier_id: 1,
-				cart,
-			});
-			alert("Transation Success");
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
 	return (
-		<Box>
-			<Button
-				onClick={onOpen}
-				w={"full"}
-				p={"14px 30px 12px 28px"}
-				borderRadius={"100px"}
-				background={"var(--brand-brand-500, #286043)"}
-				color={"var(--black-b-0, #FFF)"}
+		<Flex
+			bgColor={"var(--black-b-10, #FAFAFA)"}
+			p={"32px"}
+			gap={"44px"}
+			fontSize={"14px"}
+		>
+			{/* Left section */}
+			<Flex
+				direction={"column"}
+				w={"50%"}
+				bgColor={"white"}
+				p={"32px 52px"}
+				gap={"34px"}
 			>
-				Proceed Payment
-				<IconArrowRight />
-			</Button>
+				<Flex w={"full"} justify={"space-between"}>
+					<Flex direction={"column"} gap={"24px"}>
+						<Text>No SBX1316513</Text>
+						<Flex>
+							<IconUser width={"24px"} height={"24px"} /> {state}
+						</Flex>
+					</Flex>
 
-			<Modal isOpen={isOpen} onClose={onClose}>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader>Transaction</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody>
-						{cart?.map((items, index) => {
-							return (
-								<Box>
-									<Flex key={index} gap={5} justify={"space-between"}>
-										<Box>{items?.name}</Box>
-										<Box>{items?.total}x</Box>
+					<Flex direction={"column"} gap={"24px"}>
+						<Flex gap={2}>
+							<IconCalendar />
+							<Text>
+								{day} {months[month]} {year}
+							</Text>
+						</Flex>
+						<Flex gap={2}>
+							<IconCalendar />
+							<Text>{time}</Text>
+						</Flex>
+					</Flex>
+				</Flex>
+
+				{/* Product */}
+				<Box
+					h={"280px"}
+					overflowX={"auto"}
+					sx={{
+						"::-webkit-scrollbar": {
+							display: "none",
+						},
+					}}
+				>
+					{cart?.map((items, index) => {
+						return (
+							<Box
+								key={index}
+								display={"flex"}
+								h={"fit-content"}
+								justifyContent={"space-between"}
+								flexDirection={"column"}
+								justifyItems={"center"}
+								alignItems={"flex-start"}
+								onClick={() => navigate("/")}
+							>
+								<Flex align={"center"} h={"fit-content"} w={"full"}>
+									<Flex
+										align={"center"}
+										w={"fit-content"}
+										h={"fit-content"}
+									>
+										<Image src={CoffeImg} w={"100px"} h={"80px"} />
 									</Flex>
-								</Box>
-							);
-						})}
-						<Flex justify={"end"}>{toRupiah(transactionPrice)}</Flex>
-					</ModalBody>
+									<Flex direction={"column"} w={"full"} gap={5}>
+										<Flex w={"full"} justify={"space-between"}>
+											<Text>
+												{items.name} ({items.total}x)
+											</Text>
+											<Text>
+												{toRupiah(items.initialPrice || items.price)}
+											</Text>
+										</Flex>
+									</Flex>
+								</Flex>
+							</Box>
+						);
+					})}
+					{/* <Flex justify={"end"}>{toRupiah(transactionPrice)}</Flex> */}
+				</Box>
+				<Flex
+					w={"full"}
+					direction={"column"}
+					gap={"16px"}
+					fontSize={"14px"}
+				>
+					<Flex w={"full"} justify={"space-between"}>
+						<Text>Subtotal</Text>
+						<Text>{toRupiah(transactionPrice)}</Text>
+					</Flex>
+					<Flex w={"full"} justify={"space-between"}>
+						<Text>Discount</Text>
+						<Text>-Rp 0,00</Text>
+					</Flex>
+					<Flex w={"full"} justify={"space-between"}>
+						<Text>Total Tax</Text>
+						<Text>{toRupiah(transactionPrice * (2 / 100))}</Text>
+					</Flex>
+					<Divider />
+					<Flex w={"full"} justify={"space-between"}>
+						<Text fontSize={"20px"} fontWeight={600}>
+							Total
+						</Text>
+						<Text fontSize={"20px"} fontWeight={600}>
+							{toRupiah(
+								transactionPrice + transactionPrice * (10 / 100)
+							)}
+						</Text>
+					</Flex>
+				</Flex>
+			</Flex>
 
-					<ModalFooter>
-						<Button colorScheme="blue" mr={3} onClick={onClose}>
-							Close
-						</Button>
-						<Button
-							variant="ghost"
-							onClick={() => {
-								bayar(totalQuantity, transactionPrice, cart);
-							}}
-						>
-							Bayar
-						</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
-		</Box>
+			{/* Right Section */}
+			<Flex
+				w={"50%"}
+				bgColor={"white"}
+				direction={"column"}
+				p={"32px 52px"}
+				gap={"34px"}
+			>
+				<Flex direction={"column"} gap={"30px"} h={"full"}>
+					<Text>Payment Methode</Text>
+					{activePage == "Payment" && (
+						<Payment setActive={setIsActivePage} />
+					)}
+					{activePage == "Cash" && (
+						<CashPayment
+							total={transactionPrice + transactionPrice * (10 / 100)}
+							setActive={setIsActivePage}
+							setIsPayment={setPayment}
+						/>
+					)}
+					{activePage == "PaymentSuccess" && (
+						<PaymentSuccess name={state} payment={payment} />
+					)}
+				</Flex>
+			</Flex>
+		</Flex>
 	);
 };
